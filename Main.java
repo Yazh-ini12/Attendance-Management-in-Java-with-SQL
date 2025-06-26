749c0ae6250f09b8f3d37baf631dbbc5a7349cb5
YAZHINI D <yazhini9804@gmail.com>
2025-06-26 12:00:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+package Attendance;
+
+import java.sql.SQLException;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
